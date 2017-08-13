82f2e2fd8aeac43b1317c263f749da4097d2d549
qianfeifan123 <qff@erongchuang.com>
2017-08-13 09:42:25 +0800

    update

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,10 +6,10 @@ return array(
 	'URL_ROUTER_ON'   => true, 
 	'URL_MODEL'       =>1,
 	'DB_TYPE'      =>  'mysql',//数据库类型
-	'DB_HOST'      =>  'localhost',//服务器地址
+	'DB_HOST'      =>  '39.108.89.151',//服务器地址
 	'DB_NAME'      =>  'erongchuang',//数据库名
 	'DB_USER'      =>  'root',  // 用户名
-	'DB_PWD'       =>  '',     // 密码
+	'DB_PWD'       =>  'root',     // 密码
 	'DB_PORT'      =>  '3306', // 端口
 	'DB_PREFIX'    =>  '',     // 数据库表前缀
 	'DB_CHARSET'   =>  'utf8', //数据库的编码 默认为utf8
